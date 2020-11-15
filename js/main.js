57cf6fa96130400e38d62e344c2219f1eda66975
carlosbelen <71188503+carlosbelen@users.noreply.github.com>
2020-11-15 11:13:48 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,62 +5,47 @@ function getWeather() {
     .then(response => response.json())
     .then(data => {
         console.log(data)
-     
+
      
 // City Name
-    let city_name = data.name  
-    let info1 = document.createElement('h3')
-    info1.innerHTML = city_name
-    let target1 = document.getElementById('citydisplay')
-    target1.append(info1)
+    let city_display = document.getElementById('citydisplay')
+    city_display.innerHTML = data.name
 
 
 // Current Temp
     let Current = data.main.temp
     Current = Temp_conversion(Current)
-    let target = document.getElementById('tr1')
-    let info = document.createElement('td')
-    info.innerHTML = Current
-    target.append(info)
-
+    let current_display = document.getElementById('current')
+    current_display.innerHTML = Current
+    
 // Feels Like Temp
     let Feels_like = data.main.feels_like
     Feels_like = Temp_conversion(Feels_like)
-    let targe = document.getElementById('tr1')
-    let inf = document.createElement('td')
-    inf.innerHTML = Feels_like
-    targe.append(inf)
+    let Feels_like_display = document.getElementById('feels_like')
+    Feels_like_display.innerHTML = Feels_like
     
 
 // High Temp
-    let Temp_Max = data.main.temp_max
+    let Temp_Max = data.main.temp_max   
     Temp_Max = Temp_conversion(Temp_Max)
-    let target2 = document.getElementById('tr1')
-    let info2 = document.createElement('td')
-    info2.innerHTML = Temp_Max
-    target2.append(info2)
+    let Temp_Max_display = document.getElementById('high')
+    Temp_Max_display.innerHTML = Temp_Max   
 
 // Low Temp
-    let Temp_Min = data.main.temp_min
+    let Temp_Min = data.main.temp_min   
     Temp_Min = Temp_conversion(Temp_Min)
-    let target3 = document.getElementById('tr1')
-    let info3 = document.createElement('td')
-    info3.innerHTML = Temp_Min
-    target3.append(info3)
+    let Temp_Min_display = document.getElementById('low')
+    Temp_Min_display.innerHTML = Temp_Min   
     
 // Forecast
-    let Forecast = data.weather[0].main
-    let target4 = document.getElementById('tr1')
-    let info4 = document.createElement('td')
-    info4.innerHTML = Forecast
-    target4.append(info4)
+    let Forecast = data.weather[0].main   
+    let Forecast_display = document.getElementById('forecast')
+    Forecast_display.innerHTML = Forecast   
 
 // Humididty
-    let humidity = data.main.humidity
-    let target5 = document.getElementById('tr1')
-    let info5 = document.createElement('td')
-    info5.innerHTML = humidity
-    target5.append(info5)
+    let Humidity = data.main.humidity  
+    let Humidity_display = document.getElementById('humidity')
+    Humidity_display.innerHTML = Humidity   
     })
 }
 
